8de108c581fb8be9846ba1a6632f61fe26463d40
Erez Sh <erezshin@gmail.com>
2020-01-14 12:35:56 +0200

    Updated setup.py

--- runtype/__init__.py
+++ runtype/__init__.py
@@ -1,3 +1,5 @@
 from .isa import isa, test_subclass
 from .dispatch import Dispatch, DispatchError
 from .dataclass import dataclass
+
+__version__ = "0.1a1"
--- setup.py
+++ setup.py
@@ -1,43 +1,48 @@
 import re
 from setuptools import setup
 
-# __version__ ,= re.findall('__version__ = "(.*)"', open('lark/__init__.py').read())
-__version__ = '0.1a'
+__version__ ,= re.findall('__version__ = "(.*)"', open('runtype/__init__.py').read())
 
 setup(
     name = "runtype",
     version = __version__,
-    packages = [''],
+    packages = ['runtype'],
 
     requires = [],
     install_requires = [],
 
     package_data = { '': ['*.md', '*.lark'] },
 
-    # test_suite = 'tests.__main__',
+    test_suite = 'tests.__main__',
 
     # metadata for upload to PyPI
     author = "Erez Shinan",
     author_email = "erezshin@gmail.com",
     description = "Type dispatch and validation for run-time",
     license = "MIT",
-    keywords = "types",
+    keywords = "types typing dispatch dataclass runtime",
     url = "https://github.com/erezsh/runtype",
-    # download_url = "https://github.com/erezsh/lark/tarball/master",
+    download_url = "https://github.com/erezsh/runtype/tarball/master",
     long_description='''
-    To come
+runtype is composed of several utility modules:
+
+1. dispatch - Provides a decorator for fast multi-dispatch at run-time for functions, with sophisticated ambiguity resolution.
+
+2. dataclass - Improves on Python's existing dataclass, by verifying the type-correctness of its attributes at run-time. Also provides a few useful methods for dataclasses.
+
+3. isa - Provides alternative functions to `isinstance` and `issubclass`, that undestand Python's `typing` module.
+
+Runtype's integration with the `typing` module allows to use type signatures such as `List[int]`, `Optional[str]`, or `Union[int, str, Callable]`.
 ''',
 
     classifiers=[
-        # "Development Status :: 5 - Production/Stable",
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
-        # "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Text Processing :: General",
-        # "Topic :: Text Processing :: Linguistic",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: Utilities",
         "License :: OSI Approved :: MIT License",
     ],
-
 )
 
