f625391316ca730651cc71f8882f55b7d69e950a
Erez Sh <erezshin@gmail.com>
2021-09-05 18:23:55 +0100

    Refactored Type and friends into base_types.py

---
+++ runtype/base_types.py
@@ -0,0 +1,179 @@
+"""
+Base Types - contains the basic building blocks of a generic type system
+"""
+
+class RuntypeError(TypeError):
+    pass
+
+class TypeMismatchError(RuntypeError):
+    pass
+
+class LengthMismatchError(TypeMismatchError):
+    pass
+
+class Type:
+    def __add__(self, other):
+        return SumType.create((self, other))
+    def __mul__(self, other):
+        return ProductType((self, other))
+
+    def validate_instance(self, obj):
+        raise NotImplementedError(self)
+
+    def test_instance(self, obj):
+        try:
+            self.validate_instance(obj)
+            return True
+        except TypeMismatchError as _e:
+            return False
+
+class AnyType(Type):
+    supertypes = ()
+
+    def __add__(self, other):
+        return self
+
+    def __ge__(self, other):
+        if isinstance(other, (type, Type)):
+            return True
+
+        return NotImplemented
+
+    def __le__(self, other):
+        if isinstance(other, Type):
+            return other is self
+
+        return NotImplemented
+
+    def validate_instance(self, obj):
+        return True
+
+    def __repr__(self):
+        return 'Any'
+
+
+Any = AnyType()
+
+
+class DataType(Type):
+    def __init__(self, kernel, supertypes={Any}):
+        self.kernel = kernel
+
+    def __repr__(self):
+        return str(self.kernel)  #.__name__
+
+class SumType(Type):
+    def __init__(self, types):
+        self.types = set(types)
+
+    @classmethod
+    def create(cls, types):
+        x = set()
+        for t in types:
+            if isinstance(t, SumType):
+                x |= set(t.types)
+            else:
+                x.add(t)
+
+        if len(x) == 1:
+            return list(x)[0]
+        return cls(x)
+
+    def __repr__(self):
+        return '(%s)' % '+'.join(map(repr, self.types))
+
+    def __le__(self, other):
+        return all(t <= other for t in self.types)
+
+    def __ge__(self, other):
+        if not isinstance(other, Type):
+            return NotImplemented
+
+        return any(other <= t for t in self.types)
+
+    def __eq__(self, other):
+        if not isinstance(other, SumType):
+            return NotImplemented
+        return self.types == other.types
+
+    def __hash__(self):
+        return hash(frozenset(self.types))
+
+    def validate_instance(self, obj):
+        if not any(t.test_instance(obj) for t in self.types):
+            raise TypeMismatchError(obj, self)
+
+
+class ProductType(Type):
+    def __init__(self, types):
+        self.types = tuple(types)
+
+    def __repr__(self):
+        return '(%s)' % '*'.join(map(repr, self.types))
+
+    def __hash__(self):
+        return hash(self.types)
+
+    def __eq__(self, other):
+        if not isinstance(other, ProductType):
+            return False
+        return self.types == other.types
+
+    def __le__(self, other):
+        if isinstance(other, ProductType):
+            if len(self.types) != len(other.types):
+                return False
+
+            return all(t1<=t2 for t1, t2 in zip(self.types, other.types))
+        elif isinstance(other, DataType):
+            return False
+
+        return NotImplemented
+
+    def validate_instance(self, obj):
+        if self.types and len(obj) != len(self.types):
+            raise LengthMismatchError(self, obj)
+        for type_, item in zip(self.types, obj):
+            type_.validate_instance(item)
+
+
+class GenericType(Type):
+    def __init__(self, base, item=Any):
+        assert isinstance(item, (Type, type)), item
+        self.base = base
+        self.item = item
+
+    def __repr__(self):
+        return '%r[%r]' % (self.base, self.item)
+
+    def __getitem__(self, item):
+        assert self.item is Any, self.item
+        return type(self)(self.base, item)
+
+    def __eq__(self, other):
+        if not isinstance(other, GenericType):
+            return False
+        return self.base == other.base and self.item == other.item
+
+    def __le__(self, other):
+        if isinstance(other, GenericType):
+            return self.base <= other.base and self.item <= other.item
+        elif isinstance(other, DataType):
+            return self.base <= other
+
+        return NotImplemented
+
+    def __ge__(self, other):
+        return not self <= other
+
+    def __hash__(self):
+        return hash((self.base, self.item))
+
+
+class SequenceType(GenericType):
+
+    def validate_instance(self, obj):
+        self.base.validate_instance(obj)
+        if self.item is not Any:
+            for item in obj:
+                self.item.validate_instance(item)
--- runtype/pytypes.py
+++ runtype/pytypes.py
@@ -1,3 +1,7 @@
+"""
+Python Types - contains an implementation of a Runtype type system that is parallel to the Python type system.
+"""
+
 import sys
 import collections
 from collections import abc
@@ -5,65 +9,8 @@ import typing
 
 py38 = sys.version_info >= (3,8)
 
-class Type:
-    def __add__(self, other):
-        return SumType.create((self, other))
-    def __mul__(self, other):
-        return ProductType((self, other))
+from .base_types import Type, DataType, GenericType, SequenceType, SumType, ProductType, Any, TypeMismatchError
 
-    def validate_instance(self, obj):
-        raise NotImplementedError(self)
-
-    def test_instance(self, obj):
-        try:
-            self.validate_instance(obj)
-            return True
-        except TypeMismatchError as _e:
-            return False
-
-class AnyType(Type):
-    supertypes = ()
-
-    def __add__(self, other):
-        return self
-
-    def __ge__(self, other):
-        if isinstance(other, (type, Type)):
-            return True
-
-        return NotImplemented
-
-    def __le__(self, other):
-        if isinstance(other, Type):
-            return other is self
-
-        return NotImplemented
-
-    def validate_instance(self, obj):
-        return True
-
-    def __repr__(self):
-        return 'Any'
-
-
-Any = AnyType()
-
-
-class RuntypeError(TypeError):
-    pass
-
-class TypeMismatchError(RuntypeError):
-    pass
-
-class TupleLengthError(TypeMismatchError):
-    pass
-
-class DataType(Type):
-    def __init__(self, kernel, supertypes={Any}):
-        self.kernel = kernel
-
-    def __repr__(self):
-        return str(self.kernel)  #.__name__
 
 class PythonDataType(DataType):
     def __le__(self, other):
@@ -78,6 +25,30 @@ class PythonDataType(DataType):
 
     def __repr__(self):
         return str(self.kernel.__name__)
+
+
+
+class TupleType(Type):
+    def __le__(self, other):
+        # No superclasses or subclasses
+        return isinstance(other, TupleType)
+
+    def __ge__(self, other):
+        if isinstance(other, TupleType):
+            return True
+        elif isinstance(other, DataType):
+            return False
+        elif isinstance(other, ProductType):
+            # Products are a tuple, but with length and types
+            return True
+
+        return NotImplemented
+
+    def validate_instance(self, obj):
+        if not isinstance(obj, tuple):
+            raise TypeMismatchError(obj, self)
+
+
 
 class OneOf(Type):
     def __init__(self, values):
@@ -94,146 +65,6 @@ class OneOf(Type):
         return 'Literal[%s]' % ', '.join(map(repr, self.values))
 
 
-class SumType(Type):
-    def __init__(self, types):
-        self.types = set(types)
-
-    @classmethod
-    def create(cls, types):
-        x = set()
-        for t in types:
-            if isinstance(t, SumType):
-                x |= set(t.types)
-            else:
-                x.add(t)
-
-        if len(x) == 1:
-            return list(x)[0]
-        return cls(x)
-
-    def __repr__(self):
-        return '(%s)' % '+'.join(map(repr, self.types))
-
-    def __le__(self, other):
-        return all(t <= other for t in self.types)
-
-    def __ge__(self, other):
-        if not isinstance(other, Type):
-            return NotImplemented
-
-        return any(other <= t for t in self.types)
-
-    def __eq__(self, other):
-        if not isinstance(other, SumType):
-            return NotImplemented
-        return self.types == other.types
-
-    def __hash__(self):
-        return hash(frozenset(self.types))
-
-    def validate_instance(self, obj):
-        if not any(t.test_instance(obj) for t in self.types):
-            raise TypeMismatchError(obj, self)
-
-
-class TupleType(Type):
-    def __le__(self, other):
-        # No superclasses or subclasses
-        return isinstance(other, TupleType)
-
-    def __ge__(self, other):
-        if isinstance(other, TupleType):
-            return True
-        elif isinstance(other, DataType):
-            return False
-
-        return NotImplemented
-
-    def validate_instance(self, obj):
-        if not isinstance(obj, tuple):
-            raise TypeMismatchError(obj, self)
-
-class ProductType(Type):
-    def __init__(self, types):
-        self.types = tuple(types)
-
-    def __repr__(self):
-        return '(%s)' % '*'.join(map(repr, self.types))
-
-    def __hash__(self):
-        return hash(self.types)
-
-    def __eq__(self, other):
-        if not isinstance(other, ProductType):
-            return False
-        return self.types == other.types
-
-    def __le__(self, other):
-        if isinstance(other, TupleType):
-            # Products are a tuple, but with length and types
-            return True
-        elif isinstance(other, ProductType):
-            if len(self.types) != len(other.types):
-                return False
-
-            return all(t1<=t2 for t1, t2 in zip(self.types, other.types))
-        elif isinstance(other, DataType):
-            return False
-
-        return NotImplemented
-
-    def validate_instance(self, obj):
-        if self.types and len(obj) != len(self.types):
-            raise TupleLengthError(self, obj)
-        for type_, item in zip(self.types, obj):
-            type_.validate_instance(item)
-
-
-class GenericType(Type):
-    def __init__(self, base, item=Any):
-        assert isinstance(item, (Type, type)), item
-        self.base = base
-        self.item = item
-
-    def __repr__(self):
-        return '%r[%r]' % (self.base, self.item)
-
-    def __getitem__(self, item):
-        assert self.item is Any, self.item
-        return type(self)(self.base, item)
-
-    def __eq__(self, other):
-        if not isinstance(other, GenericType):
-            return False
-        return self.base == other.base and self.item == other.item
-
-    def __le__(self, other):
-        if isinstance(other, GenericType):
-            return self.base <= other.base and self.item <= other.item
-        elif isinstance(other, DataType):
-            return self.base <= other
-        elif isinstance(other, TupleType):
-            return False    # tuples are generics, not the other way around
-
-        return NotImplemented
-
-    def __ge__(self, other):
-        return not self <= other
-
-    def __hash__(self):
-        return hash((self.base, self.item))
-
-    def validate_instance(self, obj):
-        raise NotImplementedError()
-
-
-class SequenceType(GenericType):
-
-    def validate_instance(self, obj):
-        self.base.validate_instance(obj)
-        if self.item is not Any:
-            for item in obj:
-                self.item.validate_instance(item)
 
 class DictType(GenericType):
     def __init__(self, base, item=Any*Any):
